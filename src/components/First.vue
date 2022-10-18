608a4af3595d04b907ef49181a19215c759d71ff
Luca Mastrorilli <mastrorilli.luca@gmail.com>
2022-10-19 08:24:56 +1100

    Initial commit

---
+++ src/components/First.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-6xl text-center">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "First",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <First msg="Hello Vue3 + Tailwind" />
+  </div>
+</template>
+
+<script>
+import First from "../components/First";
+
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {
+    First,
+  },
+  methods: {
+    logger: function () {
+      console.log("open event");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
